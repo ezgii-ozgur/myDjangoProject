47acb609ed6d4a706da89ec516e258a996e44bc5
ezgi.ozgur <ezgi.ozgur@mepsan.com.tr>
2022-12-05 23:29:34 +0300

    added registeration page and backend

--- blog/views.py
+++ blog/views.py
@@ -2,21 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-# posts = [
-#     {
-#         'author':'Ezgi Özgür',
-#         'title':'PRINCESS EZGI',
-#         'content':'Ezgi is a best princess in the MEPSAN',
-#         'date_posted':"July 14, 1998"
-#     },
-#     {
-#         'author':'Kasım Boydas',
-#         'title':'FRISOR KASIM ',
-#         'content':'Kasım ar baste frisor i Goteborg',
-#         'date_posted':"July 14, 1998"
-#     }
-# ]
-
 
 def home(request):
     context = {
