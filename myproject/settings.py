c9d5e2f8a0d9141e7375c3bd7c000f5e3a16fa88
ezgi.ozgur <ezgi.ozgur@mepsan.com.tr>
2022-12-06 16:16:08 +0300

    added profile.html login and logout

--- myproject/settings.py
+++ myproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- users/views.py
+++ users/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('user_name')
-            print("user_name",username)
-            messages.success(request,f'Account created for {username}')
+            form.save()
+            username = form.cleaned_data.get('username')
+            print("user_name", username)
+            messages.success(request, f'Your account has been created! You are now able to login!')
             print(messages, messages)
-            return redirect('blog-home')
+            return redirect('login')
 
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
